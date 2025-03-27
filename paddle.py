e4807f9792bfa7ce25a2c03223746387a0523cab
zulkiflirmdn <70694568+zulkiflirmdn@users.noreply.github.com>
2025-03-27 18:19:03 +0800

    initial commit

---
+++ paddle.py
@@ -0,0 +1,21 @@
+from turtle import Screen, Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.create_paddel()
+        self.shape("square")
+        self.color("White")
+        self.penup()
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.goto(position)
+
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+
