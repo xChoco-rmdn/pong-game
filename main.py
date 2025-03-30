31ab9a1860fe6d4ed76f5bfc85adc5f354ae4f07
xChoco-rmdn <zulkiflirmdn@gmail.com>
2025-03-30 22:32:06 +0800

    add ball

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import time
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
 
 screen = Screen()
 screen.bgcolor("black")
@@ -9,6 +11,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -19,6 +22,24 @@ screen.onkey(l_paddle.go_down, "s")
 game_is_on = True
 
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+
+    # Detect Collision of the Wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect Collision with the paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # Deetect r paddle misses
+    if ball.xcor() > -380:
+        ball.reset_position()
+
+    # Deetect l paddle misses
+    if ball.xcor() < 380:
+        ball.reset_position()
 
 screen.exitonclick()
